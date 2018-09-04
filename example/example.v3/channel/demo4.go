3247b4928bd69b507c3c6f0ee40217696b0bef9d
simple.xull <simple.xull@gmail.com>
2018-09-04 20:55:37 +0800

    feat: exam

---
+++ example/example.v3/channel/demo4.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	// 示例1： 只能发不能收的通道
+	var uselessChan = make(chan<- int, 1)
+	// 只能收不能发的通道
+	var anotherUselessChan = make(<-chan int, 1)
+
+	// 这里打印的是可以分别代表两个通道的指针的16进制表示
+	fmt.Printf("The useless channel: %v, %v\n", uselessChan, anotherUselessChan)
+
+	// 示例2：
+	intChan1 := make(chan int, 3)
+	SendIn(intChan1)
+
+	intChan2 := getIntChan()
+
+	// 示例4
+	for elem := range intChan2 {
+		fmt.Printf("The element in intChan2:%v\n", elem)
+	}
+
+	// 示例5
+	_ = GetIntChan(getIntChan)
+}
+
+func SendIn (ch chan<- int)  {
+	ch <- rand.Intn(1000)
+}
+
+type Notifier interface {
+	SendInt(ch chan<- int)
+}
+
+func getIntChan () <-chan int  {
+	num := 5
+	ch := make(chan int, num)
+	for i := 0; i < num; i ++ {
+		ch <- i
+	}
+	close(ch)
+	return ch
+}
+
+// 示例5
+type GetIntChan func() <-chan int
---
+++ example/example.v3/channel/demo5.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"math/rand"
+	"fmt"
+	"time"
+)
+
+func example1()  {
+	// 准备好几个通道
+	intChannel := [3]chan int {
+		make(chan int, 1),
+		make(chan int, 1),
+		make(chan int, 1),
+	}
+
+	// 随机选择一个通道，并向它发送元素值
+	index := rand.Intn(3)
+	fmt.Printf("The index: %d\n", index)
+	intChannel[index]<- index
+
+	// 哪一个通道中有可取的元素值，哪个对应的分支就会被执行
+	select {
+	case <-intChannel[0]:
+		fmt.Println("The first candidate case is selected")
+	case <-intChannel[1]:
+		fmt.Println("THe second candidate case is selected")
+	case elem := <-intChannel[2]:
+		fmt.Println("The third candidate case is selected, the element is %d.\n", elem)
+	default:
+		fmt.Println("No candidate case is selected")
+
+	}
+
+}
+
+// 示例2。
+func example2() {
+	intChan := make(chan int, 1)
+	// 一秒后关闭通道。
+	time.AfterFunc(time.Second, func() {
+		close(intChan)
+	})
+	select {
+	case _, ok := <-intChan:
+		if !ok {
+			fmt.Println("The candidate case is closed.")
+			break
+		}
+		fmt.Println("The candidate case is selected.")
+	}
+}
+
+func main() {
+	example2()
+}
