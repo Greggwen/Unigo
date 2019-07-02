b17cac91ead82ad7e1d70e0026a11c1cf0bc9d02
simple.xull <simple.xull@gmail.com>
2019-07-02 21:11:01 +0800

    reflect study

---
+++ reflect/example1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+
+	var num float64 = 1.2345
+
+	fmt.Println("The Type of Num is ", reflect.TypeOf(num)) // reflect.TypeOf() 直接给到了我们想要的Type的类型
+	fmt.Println("The Value of Num is ", reflect.ValueOf(num))  // reflect.ValueOf：直接给到了我们想要的具体的值
+
+
+	pointer := reflect.ValueOf(&num)
+	value := reflect.ValueOf(num)
+
+	fmt.Println("Pointer :", pointer)
+	fmt.Println("value : ", value)
+
+	convertPointer := pointer.Interface().(*float64)
+	convertValue := value.Interface().(float64)
+
+	fmt.Println("convertValue :", convertPointer)
+	fmt.Println("convertValue : ", convertValue)
+
+}
---
+++ reflect/example2/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	Id int
+	Name string
+	Age int
+}
+
+func (u User) Hello()  {
+	fmt.Println("Hello World!User=User")
+}
+
+func main() {
+
+	u := User{1, "Roally", 12}
+
+	Info(u)
+	
+
+}
+
+func Info(o interface{})  {
+	t := reflect.TypeOf(o)
+	fmt.Println("Type:", t.Name())
+
+	v := reflect.ValueOf(o)
+	fmt.Println("Fields:", v)
+
+	for i := 0;	i < t.NumField();  i++ {
+		f := t.Field(i)
+		val := v.Field(i).Interface()
+		fmt.Printf("%6s: %v = %v\n", f.Name, f.Type, val)
+	}
+
+	for i := 0; i < t.NumMethod(); i ++ {
+		m := t.Method(i)
+		fmt.Printf("%s: %v\n", m.Name, m.Type)
+	}
+}
