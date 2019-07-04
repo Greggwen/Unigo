624223dbed6863638f4e78ff4005701631ceb339
simple.xull <simple.xull@gmail.com>
2019-07-04 10:30:30 +0800

    Reflect study 2

---
+++ reflect/example3/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	Id int
+	Name string
+	Age int
+}
+
+func (u User) Hello(name string) {
+	fmt.Println("Hello", name, ", my name is", u.Name)
+}
+
+func main() {
+	u := User{
+		Id: 1,
+		Name: "Rollay",
+		Age: 20,
+	}
+
+	v := reflect.ValueOf(u)
+	mv := v.MethodByName("Hello")
+
+	args := []reflect.Value{reflect.ValueOf("Corwien")}
+
+	mv.Call(args)
+}
